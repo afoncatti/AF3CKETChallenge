ab8b224417b54fb217f62b2b7cbcd6bee9924d85
Alexandre Foncatti <alexandre.foncatti@gmail.com>
2023-09-16 15:45:44 +0100

    initial commit

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  customSyntax: 'postcss-html',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-vue'
+  ],
+  rules: {
+    'declaration-block-no-duplicate-properties': false
+  }
+}
